4ec44cf7bf4e7cd481a96f14f36d183274e9ad8c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-05 19:01:49 -0400

    Create Get() request

--- src/basic-reports/basic-reports.controller.ts
+++ src/basic-reports/basic-reports.controller.ts
@@ -4,4 +4,9 @@ import { BasicReportsService } from './basic-reports.service';
 @Controller('basic-reports')
 export class BasicReportsController {
   constructor(private readonly basicReportsService: BasicReportsService) {}
+
+  @Get()
+  async hello() {
+    return await this.basicReportsService.hello();
+  }
 }
