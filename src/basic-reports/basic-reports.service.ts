a2033ccf46ce871c5c0cc96d4df1525f21c0f291
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-05 20:06:58 -0400

    Print a message

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
       await this.$connect();
+      console.log("Connected to the database!");
     }
 
     async hello() {
