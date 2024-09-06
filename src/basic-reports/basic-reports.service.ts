7437496337ea8a60a4f6d41634dde1b179e0087e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-06 13:00:54 -0400

    Refactor BasicReportsService to use Prisma's findFirst method

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -5,10 +5,10 @@ import { PrismaClient } from '@prisma/client';
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
       await this.$connect();
-      console.log("Connected to the database!");
+      // console.log("Connected to the database!");
     }
 
     async hello() {
-        return 'Hello from BasicReportsService!';
+        return this.employees.findFirst();
     }
 }
